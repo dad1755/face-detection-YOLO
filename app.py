a69c97e7396ecb11ec2d52cb7a59eaa2c1603f51
dad1755 <98881062+dad1755@users.noreply.github.com>
2024-10-15 13:06:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if 'model' not in st.session_state:
 
 # Create a form for input and submission
 with st.form(key='query_form', clear_on_submit=True):
-    user_query = st.text_input("Dont have image to count face ? Submit your query 💬 📚)", placeholder="Enter your query here...", max_chars=200)
+    user_query = st.text_input("Dont have image to count face ? Submit your query 💬 📚", placeholder="Enter your query here...", max_chars=200)
     submit_button = st.form_submit_button("Submit")
 
 # Add a file uploader for document and image
